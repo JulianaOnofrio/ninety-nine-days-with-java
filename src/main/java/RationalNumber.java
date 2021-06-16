101d64331053e9c96df2234f0bb67d19faec1941
JulianaOnofrio <julianaonofrio@hotmail.com>
2021-06-16 03:16:44 -0300

    Implementing TDD

--- src/main/java/RationalNumber.java
+++ src/main/java/RationalNumber.java
@@ -3,8 +3,19 @@ public class RationalNumber {
     private final Integer numerator;
     private final Integer denominator;
 
-    public RationalNumber(final Integer numerator,
-                          final Integer denominator) {
+    public RationalNumber(
+            final Integer numerator,
+            final Integer denominator
+    ) {
+        if(numerator==null) {
+            throw new IllegalArgumentException("Must not accept null numerator");
+        }
+        if(denominator==null){
+            throw new IllegalArgumentException("Must not accept null denominator");
+        }
+        if(Integer.valueOf(0).equals(denominator)) {
+            throw new IllegalArgumentException("Must not accept denominator equal to 0");
+        }
         this.numerator = numerator;
         this.denominator = denominator;
     }
