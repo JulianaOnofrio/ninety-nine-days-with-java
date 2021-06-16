e2ac94ec907f32d702fa54e3a1b51d01b16e67ca
JulianaOnofrio <julianaonofrio@hotmail.com>
2021-06-16 03:10:01 -0300

    Representing rational numbers

---
+++ src/main/java/RationalNumber.java
@@ -0,0 +1,12 @@
+public class RationalNumber {
+
+    private final Integer numerator;
+    private final Integer denominator;
+
+    public RationalNumber(final Integer numerator,
+                          final Integer denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+}
+
--- src/test/java/RationalNumberTest.java
+++ src/test/java/RationalNumberTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class RationalNumberTest {
+
+    @Test
+    void test() {
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new RationalNumber(1, 0),
+                "Must not accept denominator equal to 0");
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new RationalNumber(null, 0),
+                "Must not accept null numerator");
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new RationalNumber(1, null),
+                "Must not accept null denominator");
+    }
 }
